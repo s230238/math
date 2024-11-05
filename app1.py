d3ce905a1c901ef0f7679077a013567639c737dc
s230238 <gimpo.s230238@ggh.goe.go.kr>
2024-11-05 16:27:39 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,14 +9,14 @@ st.subheader('오늘의 주제: 양수와 음수')
 st.write('음수?? 어렵지 않아!') 
 
 # 여러 개의 열(문단)을 생성
-#col1, col2 = st.columns(2)       
+col1, col2 = st.columns(2)       
 # 왼쪽 문단
-#with col1:
-#      st.subheader('**개념정리')
-#      st.write('- 양수: 0보다 큰 수')
-#      st.write('- 음수: 0보다 작은 수')
-#      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
-#      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
+with col1:
+      st.subheader('**개념정리')
+      st.write('- 양수: 0보다 큰 수')
+      st.write('- 음수: 0보다 작은 수')
+      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
+      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 # 오른쪽 문단
 #with col2:
 #      st.image('수학이미지.PNG')  # 이미지 파일 불러오기
@@ -25,10 +25,10 @@ st.write('음수?? 어렵지 않아!')
 #a = st.number_input('____________', value= ____)  
 
 # 버튼 생성 및 동작
-#if st.button('양수일까 ____일까?'):
+#if st.button('양수일까 음수일까??'):
 #       if a > 0:
-#              st.write('________')
+#              st.write('양수입니다')
 #       elif a < 0:
-#              st.write('________')
+#              st.write('음수입니다')
 #       else:
-#              st.write('________')
+#              st.write('0입니다')
